c1bb530f10ba186fcf80b2a6c48ac34749b71266
Kasraoui Anouar <med.anouar91@gmail.com>
2022-04-09 18:58:18 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import Profile from './profile/Profile';
+import telechargement from "./img/telechargement.jpg"
 import './App.css';
 
+
+
 function App() {
+  const appName ="john";
+  const appBio ="snow";
+  const appProfession ="king"
+  const handleName =(name)=>{
+    alert (`bonjour ${name}`);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+       <Profile name={appName} bio= {appBio} profession={appProfession} handleName ={()=>handleName(appName)}>
+          <img src={telechargement}/>
+        </Profile> 
+       
     </div>
+    
   );
 }
 
---
+++ src/profile/Profile.js
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types';
+
+function Profile(props) {
+    return(
+        <div style={{margin: 50}}>
+        <div >
+ 
+            {props.children}
+            
+        </div>
+        <div>
+        <div>{props.name}</div>
+        <div>{props.bio}</div>
+        <div>{props.profession}</div>
+        </div>
+        <div>
+            <button onClick={props.handleName} >click me</button>
+        </div>
+        </div>
+    )    
+};
+
+Profile.propTypes = {
+    name: PropTypes.string,
+    bio: PropTypes.string,
+    profession: PropTypes.string
+
+  };
+export default Profile;
