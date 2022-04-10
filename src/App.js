06778c81a433ef090bfa3ff313aae78270484962
Kasraoui Anouar <med.anouar91@gmail.com>
2022-04-10 15:51:32 +0200

    props

--- src/App.js
+++ src/App.js
@@ -5,16 +5,16 @@ import './App.css';
 
 
 function App() {
-  const appName ="john";
-  const appBio ="snow";
-  const appProfession ="king"
+  const appName ="Kasraoui Anouar";
+  const appBio ="full stuck";
+  const appProfession ="developer "
   const handleName =(name)=>{
     alert (`bonjour ${name}`);
   }
   return (
     <div>
        <Profile name={appName} bio= {appBio} profession={appProfession} handleName ={()=>handleName(appName)}>
-          <img src={telechargement}/>
+          <img style={{width:'250px', borderRadius:'90 px ',marginTop:'50px',marginLeft:'50px'}} src={telechargement}/>
         </Profile> 
        
     </div>
--- src/profile/Profile.js
+++ src/profile/Profile.js
@@ -3,22 +3,27 @@ import PropTypes from 'prop-types';
 function Profile(props) {
     return(
         <div style={{margin: 50}}>
-        <div >
- 
-            {props.children}
-            
-        </div>
-        <div>
-        <div>{props.name}</div>
-        <div>{props.bio}</div>
-        <div>{props.profession}</div>
-        </div>
-        <div>
-            <button onClick={props.handleName} >click me</button>
-        </div>
+            <div >
+                {props.children}
+            </div>
+            <div>
+                <div>{props.name}</div>
+                <div>{props.bio}</div>
+                <div>{props.profession}</div>
+            </div>
+            <div>
+                <button onClick={props.handleName} >click me</button>
+            </div>
         </div>
     )    
 };
+
+Profile.defaultProps={
+    name:'username',
+    bio:'description',
+    profession:'profession',
+    children:'image'
+}
 
 Profile.propTypes = {
     name: PropTypes.string,
